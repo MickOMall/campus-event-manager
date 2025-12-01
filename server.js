458b36694e8210f24a065f64be8b0c9294aaeb2b
MickOMall <momalley@muhlenberg.edu>
2025-11-30 20:16:09 -0500

    Initialize Express server with MongoDB connection

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const methodOverride = require('method-override'); // HTML ejs override
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.error(err));
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method')); 
+
+// Set up EJS views
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Routes
+const eventRoutes = require('./routes/events');
+app.use('/', eventRoutes);
+
+// Start server
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
