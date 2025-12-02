b33173316ce1f8fff56f2d0f36c97afa97441897
MickOMall <momalley@muhlenberg.edu>
2025-12-01 19:23:11 -0500

    Update events.js

--- routes/events.js
+++ routes/events.js
@@ -2,37 +2,56 @@ const express = require('express');
 const router = express.Router();
 const Event = require('../models/Event');
 
-// GET all events
+// GET all events with optional sorting
 router.get('/', async (req, res) => {
-  const events = await Event.find().sort({ date: 1 });
-  res.render('index', { events });
+  try {
+    const sortBy = req.query.sort || 'createdAt'; // default sort by order entered
+    let sortOption = {};
+
+    if (sortBy === 'date') {
+      sortOption.date = 1; // ascending by event date
+    } else {
+      sortOption.createdAt = 1; // ascending by creation time
+    }
+
+    const events = await Event.find().sort(sortOption);
+    res.render('index', { events, currentSort: sortBy }); // pass currentSort to EJS
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
 });
 
-// POST create new event
+// POST new event
 router.post('/events', async (req, res) => {
   try {
-    await Event.create(req.body); // date will cast automatically
+    await Event.create(req.body);
     res.redirect('/');
   } catch (err) {
     console.error(err);
-    res.status(500).send('Server error');
+    res.status(500).send('Error creating event');
   }
 });
 
 // GET edit form
 router.get('/events/:id/edit', async (req, res) => {
-  const event = await Event.findById(req.params.id);
-  res.render('edit', { event });
+  try {
+    const event = await Event.findById(req.params.id);
+    res.render('edit', { event });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching event');
+  }
 });
 
 // PUT update event
 router.put('/events/:id', async (req, res) => {
   try {
-    await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    await Event.findByIdAndUpdate(req.params.id, req.body);
     res.redirect('/');
   } catch (err) {
     console.error(err);
-    res.status(500).send('Server error');
+    res.status(500).send('Error updating event');
   }
 });
 
@@ -43,7 +62,7 @@ router.delete('/events/:id', async (req, res) => {
     res.redirect('/');
   } catch (err) {
     console.error(err);
-    res.status(500).send('Server error');
+    res.status(500).send('Error deleting event');
   }
 });
 
